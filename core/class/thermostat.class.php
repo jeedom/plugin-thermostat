3a0d15149f8f7e3aa0e85d2af43f287e6edcd796
Loïc <loic@jeedom.com>
2017-11-28 13:29:48 +0100

    typo

--- core/class/thermostat.class.php
+++ core/class/thermostat.class.php
@@ -538,7 +538,7 @@ class thermostat extends eqLogic {
 		$temp_out = $this->getCmd(null, 'temperature_outdoor')->execCmd();
 		$temp_in = $this->getCmd(null, 'temperature')->execCmd();
 		if (!is_numeric($temp_out)) {
-			log::add('thermostat', 'debug', $this->getHumanName() . ' : Attention température exterieur incorrection : ' . $temp_out);
+			log::add('thermostat', 'debug', $this->getHumanName() . ' : Attention température exterieur erroné : ' . $temp_out);
 			$temp_out = $_consigne;
 		}
 
