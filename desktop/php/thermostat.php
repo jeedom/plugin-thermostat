7ceeb9b1e1fd2605d9ae1c7b74ca3b283acda11f
Loïc <loic@jeedom.com>
2017-12-12 17:46:46 +0100

    Add research bar

--- desktop/php/thermostat.php
+++ desktop/php/thermostat.php
@@ -22,23 +22,26 @@ foreach ($eqLogics as $eqLogic) {
    </div>
 
    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-    <legend>{{Mes thermostats}}
-    </legend>
+    <legend>{{Gestion}}</legend>
     <div class="eqLogicThumbnailContainer">
-      <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-       <center>
-        <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-    </center>
-    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+        <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+         <center>
+            <i class="fa fa-plus-circle" style="font-size : 5em;color:#94ca02;"></i>
+        </center>
+        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+    </div>
 </div>
-<?php
+<legend>{{Mes thermostats}}</legend>
+<input class="form-control" placeholder="{{Rechercher}}" style="margin-bottom:4px;" id="in_searchEqlogic" />
+<div class="eqLogicThumbnailContainer">
+    <?php
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 	echo "<center>";
 	echo '<img src="plugins/thermostat/doc/images/thermostat_icon.png" height="105" width="95" />';
 	echo "</center>";
-	echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+	echo '<span class="name" style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
 	echo '</div>';
 }
 ?>
@@ -47,18 +50,18 @@ foreach ($eqLogics as $eqLogic) {
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
 
-   <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-   <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-   <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
-   <ul class="nav nav-tabs" role="tablist">
-     <li role="presentation"><a class="eqLogicAction cursor" aria-controls="home" role="tab" data-action="returnToThumbnailDisplay" style="padding:10px 5px !important"><i class="fa fa-arrow-circle-left"></i></a></li>
-     <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab" style="padding:10px 5px !important"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
-     <li role="presentation"><a href="#configureAction" data-toggle="tab" style="padding:10px 5px !important"><i class="fa fa-hand-paper-o" aria-hidden="true"></i> {{Actions}}</a></li>
-     <li role="presentation"><a href="#configureMode" data-toggle="tab" style="padding:10px 5px !important"><i class="fa fa-modx" aria-hidden="true"></i> {{Modes}}</a></li>
-     <li role="presentation"><a href="#configureWindows" data-toggle="tab" style="padding:10px 5px !important"><i class="icon jeedom-fenetre-ouverte"></i> {{Ouvertures}}</a></li>
-     <li role="presentation"><a href="#configureFailure" data-toggle="tab" style="padding:10px 5px !important"><i class="fa fa-thermometer-empty" aria-hidden="true"></i> {{Défaillance sonde}}</a></li>
-     <li role="presentation"><a href="#configureFailureActuator" data-toggle="tab" style="padding:10px 5px !important"><i class="icon techno-heating3"></i>  {{Défaillance chauffage}}</a></li>
-     <?php
+ <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+ <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+ <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+ <ul class="nav nav-tabs" role="tablist">
+   <li role="presentation"><a class="eqLogicAction cursor" aria-controls="home" role="tab" data-action="returnToThumbnailDisplay" style="padding:10px 5px !important"><i class="fa fa-arrow-circle-left"></i></a></li>
+   <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab" style="padding:10px 5px !important"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+   <li role="presentation"><a href="#configureAction" data-toggle="tab" style="padding:10px 5px !important"><i class="fa fa-hand-paper-o" aria-hidden="true"></i> {{Actions}}</a></li>
+   <li role="presentation"><a href="#configureMode" data-toggle="tab" style="padding:10px 5px !important"><i class="fa fa-modx" aria-hidden="true"></i> {{Modes}}</a></li>
+   <li role="presentation"><a href="#configureWindows" data-toggle="tab" style="padding:10px 5px !important"><i class="icon jeedom-fenetre-ouverte"></i> {{Ouvertures}}</a></li>
+   <li role="presentation"><a href="#configureFailure" data-toggle="tab" style="padding:10px 5px !important"><i class="fa fa-thermometer-empty" aria-hidden="true"></i> {{Défaillance sonde}}</a></li>
+   <li role="presentation"><a href="#configureFailureActuator" data-toggle="tab" style="padding:10px 5px !important"><i class="icon techno-heating3"></i>  {{Défaillance chauffage}}</a></li>
+   <?php
 try {
 	$plugin = plugin::byId('calendar');
 	if (is_object($plugin)) {
@@ -115,7 +118,7 @@ foreach (object::all() as $object) {
         <div class="col-sm-6">
             <form class="form-horizontal">
                 <fieldset>
-                 <div class="form-group">
+                   <div class="form-group">
                     <label class="col-sm-3 control-label">{{Moteur}}</label>
                     <div class="col-sm-6">
                         <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="engine" placeholder="" >
